ac5a13aaf8d1174242fb05cfe28b675c655daa12
Denis Perov <denis.perov@gmail.com>
2017-02-19 11:25:32 +0300

    update package info

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'majus:helpers-core',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Support package for Blaze helpers package collection.',
+  summary: 'Utility for Blaze helpers.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/imajus/meteor-helpers-core',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
