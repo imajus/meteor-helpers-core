7e4814152bea239fa7476b20509592ddb01d19db
Denis Perov <denis.perov@gmail.com>
2017-02-19 11:48:54 +0300

    update package info

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'majus:helpers-core',
+  name: 'imajus:helpers-core',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Utility for Blaze helpers.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/imajus/meteor-helpers-core',
+  git: 'https://github.com/imajus/meteor-helpers-core.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null//'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.2');
-  api.use('ecmascript');
-  api.use(['templating', 'spacebars', 'underscore'], 'client');
+  api.use('ecmascript@0.6.3');
+  api.use(['templating@1.3.0', 'spacebars@1.0.13', 'underscore@1.0.10'], 'client');
   api.mainModule('helpers-core.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use(['templating', 'blaze', 'majus:helpers-core'], 'client');
+  api.use('ecmascript@0.6.3');
+  api.use('tinytest@1.0.12');
+  api.use(['templating@1.3.0', 'blaze@2.3.0', 'imajus:helpers-core@0.0.1'], 'client');
   api.mainModule('helpers-core-tests.js', 'client');
 });
